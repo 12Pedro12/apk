ba441c360b5dc948aa5464e43af5f6795e2a37c0
Pedro Luis <pedroluise16@gmail.com>
2025-12-08 23:25:07 -0400

    08-12-2025.2

--- administrativo/serializers.py
+++ administrativo/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from inteligencia_artificial.administrativo.models import Rol
 
-class RolSerializar(serializers.ModelSerializer):
+from .models import Rol
+from .models import User
+
+class RolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rol 
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    rol_id = serializers.PrimaryKeyRelatedField(
+        queryset = Rol.objects.all(),
+        source = 'rol', 
+        write_only = True
+        )
+    rol = serializers.StringRelatedField(read_only = True)
+    class Meta:
+        model = User
+        Fields = ['id', 'username', 'foto', 'correo', 'rol_id', 'rol']
+        extra_kwargs ={'password': {'write_only': True}}
--- administrativo/views.py
+++ administrativo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
 
+from .models import Rol
+from .serializers import RolSerializer
+from .models import Rol,User
+from .serializers import RolSerializer, UserSerializer
 # Create your views here.
+
+
+
+class RolViewSet(viewsets.ModelViewSet):
+    queryset = Rol.objects.all()
+    serializer_class = RolSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
